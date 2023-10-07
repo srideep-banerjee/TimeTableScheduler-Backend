a0bda4b7b3582ed3435acdfad6a5675b30452151
srideep-banerjee <banerjee.srideep@gmail.com>
2023-10-07 15:40:57 +0530

    Replaced JavaFX library with JCEF library

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,30 @@
 package org.example;
 
+import me.friwi.jcefmaven.CefAppBuilder;
+import me.friwi.jcefmaven.CefInitializationException;
+import me.friwi.jcefmaven.MavenCefAppHandlerAdapter;
+import me.friwi.jcefmaven.UnsupportedPlatformException;
+import me.friwi.jcefmaven.impl.progress.ConsoleProgressHandler;
+import org.cef.CefApp;
+import org.cef.CefClient;
+import org.cef.CefSettings;
+import org.cef.browser.CefBrowser;
+import org.cef.browser.CefBrowserFactory;
+import org.cef.handler.CefAppHandlerAdapter;
+import org.cef.handler.CefLoadHandlerAdapter;
+import org.cef.handler.CefLoadHandler.ErrorCode;
+import org.cef.network.CefCookieManager;
+import javax.swing.JFrame;
+
 import org.example.dao.SubjectDao;
 import org.example.dao.TeacherDao;
 import org.example.files.SavesHandler;
 import org.example.network.ApiActionHelper;
 import org.example.network.LocalServer;
 
-import java.awt.Desktop;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.net.URI;
 
 public class Main {
@@ -30,12 +48,44 @@ public class Main {
         String saveName = SavesHandler.getCurrentSave();
         if (saveName != null) SavesHandler.load(saveName);
 
-        try {
+        /*try {
             Desktop.getDesktop().browse(new URI(ls.getDefaultURL()));
         } catch (Exception e) {
             System.out.println(e);
+        }*/
+
+        //Create a new CefAppBuilder instance
+        CefAppBuilder builder = new CefAppBuilder();
+
+        //Check if jcef is installed, if so skip installation
+        File jcefInstallDir=new File("jcef-bundle");
+        if(jcefInstallDir.exists())builder.setSkipInstallation(true);
+
+        //Configure the builder instance
+        builder.getCefSettings().windowless_rendering_enabled = false;
+
+        builder.setAppHandler(new MavenCefAppHandlerAdapter(){});
+
+        //Build a CefApp instance using the configuration above
+        CefApp app=null;
+        try {
+            app = builder.build();
+        } catch (IOException | InterruptedException | CefInitializationException | UnsupportedPlatformException e) {
+            System.out.println(e);
+            System.exit(0);
         }
-        while(true);
-        //JavaFXApplication.launchURL(ls.getDefaultURL());
+
+        // Create a CefClient instance
+        CefClient client = app.createClient();
+
+        // Create a CefBrowser instance
+        CefBrowser browser = client.createBrowser(ls.getDefaultURL(), false, false);
+
+        // Create a JFrame to host the browser
+        JFrame frame = new JFrame("Time Table Scheduler");
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(browser.getUIComponent(), BorderLayout.CENTER);
+        frame.setVisible(true);
     }
 }
