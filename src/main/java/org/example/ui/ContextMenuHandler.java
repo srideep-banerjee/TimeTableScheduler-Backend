3aa0b1502d97b88e672e4649eff4f9ea55cc2a37
srideep-banerjee <banerjee.srideep@gmail.com>
2023-10-08 19:43:35 +0530

    Added dev tools to right click menu

--- src/main/java/org/example/ui/ContextMenuHandler.java
+++ src/main/java/org/example/ui/ContextMenuHandler.java
@@ -9,11 +9,14 @@ import java.awt.*;
 import java.net.URI;
 
 public class ContextMenuHandler extends CefContextMenuHandlerAdapter {
-    private final int OPEN_IN_BROWSER = 1;
-    private JcefLauncher jcefLauncher;
+    private final int INSPECT = 1;
+    private final int OPEN_IN_BROWSER = 2;
+    String url;
+    private CefBrowser devTools;
 
-    public ContextMenuHandler(JcefLauncher jcefLauncher) {
-        this.jcefLauncher = jcefLauncher;
+    public ContextMenuHandler(CefBrowser devTools, String url) {
+        this.devTools = devTools;
+        this.url = url;
     }
 
     @Override
@@ -24,6 +27,9 @@ public class ContextMenuHandler extends CefContextMenuHandlerAdapter {
         model.clear();
 
         // Add a custom menu item
+        model.addItem(INSPECT, "Inspect");
+        model.setEnabled(INSPECT, true);
+
         model.addItem(OPEN_IN_BROWSER, "Open in browser");
         model.setEnabled(OPEN_IN_BROWSER, true);
     }
@@ -34,9 +40,20 @@ public class ContextMenuHandler extends CefContextMenuHandlerAdapter {
     ) {
         // Register a listener for custom menu item clicks
         switch (commandId) {
+            case INSPECT -> {
+                /*try {
+                    Desktop.getDesktop().browse(new URI(jcefLauncher.url));
+                } catch (Exception e) {
+                    System.out.println(e);
+                    return false;
+                }
+                return true;*/
+                SingletonDevTools.getInstance().launch(devTools);
+                return true;
+            }
             case OPEN_IN_BROWSER -> {
                 try {
-                    Desktop.getDesktop().browse(new URI(jcefLauncher.url));
+                    Desktop.getDesktop().browse(new URI(url));
                 } catch (Exception e) {
                     System.out.println(e);
                     return false;
@@ -45,5 +62,9 @@ public class ContextMenuHandler extends CefContextMenuHandlerAdapter {
             }
         }
         return false;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
 }
