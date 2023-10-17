974e09553226ea8ef944ab1d45be3f61641793b1
srideep-banerjee <banerjee.srideep@gmail.com>
2023-10-17 10:42:04 +0530

    Modified DayPeriod and SemesterSection to be hashable

--- src/main/java/org/example/algorithms/DayPeriod.java
+++ src/main/java/org/example/algorithms/DayPeriod.java
@@ -2,18 +2,23 @@ package org.example.algorithms;
 
 import org.example.pojo.ScheduleStructure;
 
+import java.util.Objects;
+
 public class DayPeriod {
     public final byte day;
     public final byte period;
+    private final int hashCode;
 
     public DayPeriod(byte day, byte period) {
         this.day = day;
         this.period = period;
+        this.hashCode = Objects.hash(day, period);
     }
 
     public DayPeriod(short compact) {
         this.day = (byte) (compact / ScheduleStructure.getInstance().getPeriodCount());
         this.period = (byte) (compact % ScheduleStructure.getInstance().getPeriodCount());
+        this.hashCode = Objects.hash(day, period);
     }
 
     public short getCompact() {
@@ -22,5 +27,18 @@ public class DayPeriod {
 
     public static short getCompact(byte day, byte period) {
         return (short) (day * ScheduleStructure.getInstance().getPeriodCount() + period);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DayPeriod other = (DayPeriod) o;
+        return other.day == this.day && other.period == this.period;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.hashCode;
     }
 }
--- src/main/java/org/example/algorithms/SemesterSection.java
+++ src/main/java/org/example/algorithms/SemesterSection.java
@@ -3,20 +3,38 @@ package org.example.algorithms;
 public class SemesterSection {
     public final byte semester;
     public final byte section;
+    private final int hashCode;
 
     public SemesterSection(byte semester, byte section) {
         this.semester = semester;
         this. section = section;
+        this.hashCode = toString().hashCode();
     }
     public SemesterSection(String str) {
         int ind = str.indexOf(',');
         this.semester = Byte.parseByte(str.substring(0,ind));
         this.section = Byte.parseByte(str.substring(ind+1));
+        this.hashCode = toString().hashCode();
     }
+
     public String toString() {
         return semester+","+section;
     }
+
     public static String toString(byte semester, byte section) {
         return semester+","+section;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SemesterSection other = (SemesterSection) o;
+        return other.semester == this.semester && other.section == this.section;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.hashCode;
     }
 }
