a102ba2cb21d94189acedeb23ab01881f1f24cd1
srideep-banerjee <banerjee.srideep@gmail.com>
2023-10-20 20:21:30 +0530

    Added getTheoryOfPractical() and getPracticalOfTheory() methods to SubjectDao

--- src/main/java/org/example/dao/SubjectDao.java
+++ src/main/java/org/example/dao/SubjectDao.java
@@ -17,6 +17,22 @@ public class SubjectDao extends HashMap<String, Subject> {
         return instance;
     }
 
+    public String getTheoryOfPractical(String code) {
+        code = code.toUpperCase();
+        if (!containsKey(code)) return null;
+        String theory = code.replace('9', '0');
+        if (!containsKey(theory) || theory.equals(code)) return null;
+        return theory;
+    }
+
+    public String getPracticalOfTheory(String code) {
+        code = code.toUpperCase();
+        if (!containsKey(code)) return null;
+        String practical = code.replace('0', '9');
+        if (!containsKey(practical) || practical.equals(code)) return null;
+        return practical;
+    }
+
     @Override
     public Subject put(String key, Subject value) {
         return super.put(key.toUpperCase(), value);
