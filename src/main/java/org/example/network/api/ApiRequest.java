e33280d494c1e4dd339f39f65f6575b161d9e624
srideep-banerjee <banerjee.srideep@gmail.com>
2025-01-21 09:36:33 +0530

    Added request body to ApiRequest for better reusability
    (cherry picked from commit 44f69914c2c65c8129ca112bc3630e4302f351d4)

--- src/main/java/org/example/network/api/ApiRequest.java
+++ src/main/java/org/example/network/api/ApiRequest.java
@@ -2,12 +2,13 @@ package org.example.network.api;
 
 import com.sun.net.httpserver.HttpExchange;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public record ApiRequest(String path, String method, Map<String, String> queries) {
+public record ApiRequest(String path, String method, Map<String, String> queries, String body) {
 
-    public static ApiRequest fromHttpExchange(HttpExchange exchange) {
+    public static ApiRequest fromHttpExchange(HttpExchange exchange) throws IOException {
         String path = exchange.getRequestURI().getPath();
         String method = exchange.getRequestMethod();
         String queryString = exchange.getRequestURI().getQuery();
@@ -18,6 +19,7 @@ public record ApiRequest(String path, String method, Map<String, String> queries
                 queries.put(queryData[0].toLowerCase(), queryData[1].toLowerCase());
             }
         }
-        return new ApiRequest(path, method, queries);
+        String body = new String(exchange.getRequestBody().readAllBytes());
+        return new ApiRequest(path, method, queries, body);
     }
 }
