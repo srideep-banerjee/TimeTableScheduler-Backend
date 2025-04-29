0d63d3b0fa4db250a70fe358186a4317db170930
srideep-banerjee <banerjee.srideep@gmail.com>
2025-04-29 21:42:41 +0530

    Updated local config to modify db only on save
    (cherry picked from commit 0935ccd02d265a2732f08362c7877e6377f667bc)

--- src/main/java/org/example/files/db/entities/implementation/ConfigEntity.java
+++ src/main/java/org/example/files/db/entities/implementation/ConfigEntity.java
@@ -1,11 +1,15 @@
 package org.example.files.db.entities.implementation;
 
+import org.example.dao.ConfigDao;
 import org.example.files.db.CreateTableQueryBuilder;
+import org.example.files.db.InsertStatement;
 import org.example.files.db.entities.Entity;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Map;
 
 public class ConfigEntity implements Entity {
     @Override
@@ -18,7 +22,9 @@ public class ConfigEntity implements Entity {
     }
 
     @Override
-    public void clearMemory() {}
+    public void clearMemory() {
+        ConfigDao.getInstance().clear();
+    }
 
     @Override
     public void clearDataFromCurrent(Statement statement) throws SQLException {
@@ -26,8 +32,26 @@ public class ConfigEntity implements Entity {
     }
 
     @Override
-    public void loadFromCurrent(Statement statement) {}
+    public void loadFromCurrent(Statement statement) throws SQLException {
+        ConfigDao configDao = ConfigDao.getInstance();
+
+        try (ResultSet resultSet = statement.executeQuery("SELECT * FROM current.config")) {
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                String data = resultSet.getString("data");
+
+                configDao.put(name, data);
+            }
+        }
+    }
 
     @Override
-    public void saveToCurrent(Connection connection) {}
+    public void saveToCurrent(Connection connection) throws SQLException {
+        for (Map.Entry<String, String> entry: ConfigDao.getInstance().entrySet()) {
+            new InsertStatement(connection, "current.config")
+                    .add(entry.getKey())
+                    .add(entry.getValue())
+                    .executeInsert();
+        }
+    }
 }
